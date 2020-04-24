5a8cafa4dfbfdf2f73728901f28d18fb0cd40021
taityo <taicho.nv@gmail.com>
2020-04-24 10:04:48 +0000

    finished creating dev environt

--- ros-base/entry.sh
+++ ros-base/entry.sh
@@ -2,9 +2,9 @@
 set -e
 
 # setup ros environment
-source /opt/ros/$ROS_DISTRO/setup.sh
-source /home/catkin_ws/devel/setup.sh
-export ROS_PACKAGE_PATH=\${ROS_PACKAGE_PATH}:/home/catkin_ws
+source /opt/ros/$ROS_DISTRO/setup.bash
+source /home/catkin_ws/devel/setup.bash
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:/home/catkin_ws
 export ROS_WORKSPACE=/home/catkin_ws
 
 exec "$@"
