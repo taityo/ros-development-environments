a11686dbe7288dd7d49ef77f57126422be4c566f
taityo <taicho.nv@gmail.com>
2020-04-25 10:00:54 +0000

    fix moveit and moveit desktop

--- ros-moveit/entry.sh
+++ ros-moveit/entry.sh
@@ -3,8 +3,9 @@ set -e
 
 # setup ros environment
 source /opt/ros/$ROS_DISTRO/setup.bash
+source /home/catkin_ws/devel/setup.bash
 source /home/ws_moveit/devel/setup.bash
-#export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH
-export ROS_WORKSPACE=/home/ws_moveit
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:/home/catkin_ws:/home/ws_moveit
+export ROS_WORKSPACE=$WORKDIR
 
 exec "$@"
